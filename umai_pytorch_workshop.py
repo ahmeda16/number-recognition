54d93eaabee307bcc9e5e7cba79cbc68173decc7
Syed Abraham Ahmed <syed.abraham14@gmail.com>
2023-10-14 14:02:03 -0500

    Main File & Imports

---
+++ umai_pytorch_workshop.py
@@ -0,0 +1,10 @@
+## Imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms as T
+
+
+
